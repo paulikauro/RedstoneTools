5f2dc9d4cb45b17dd335174f172e4522fa4473fc
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-03-27 00:10:25 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "org.openredstone"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.3.71"
+    kotlin("kapt") version "1.3.71"
+    id("com.github.johnrengelman.shadow") version "2.0.4"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        name = "sonatype-oss"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        name = "enginehub-maven"
+        url = uri("http://maven.enginehub.org/repo/")
+    }
+    maven {
+        name = "aikar"
+        url = uri("https://repo.aikar.co/content/groups/aikar/")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(group = "co.aikar", name = "acf-paper", version = "0.5.0-SNAPSHOT")
+
+    compileOnly(group = "org.spigotmc", name = "spigot-api", version = "1.14.4-R0.1-SNAPSHOT")
+    // compileOnly(group = "org.spigotmc", name = "plugin-annotations", version = "1.2.2-SNAPSHOT")
+    compileOnly(group = "com.sk89q.worldedit", name = "worldedit-bukkit", version = "7.0.1-SNAPSHOT")
+
+    kapt(group = "org.spigotmc", name = "plugin-annotations", version = "1.2.2-SNAPSHOT")
+}
+
+tasks.shadowJar {
+    relocate("co.aikar.commands", "org.openredstone.redstonetools.acf")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
---
+++ src/main/kotlin/RedstoneTools.kt
@@ -0,0 +1,20 @@
+package org.openredstone.redstonetools
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class RedstoneTools : JavaPlugin(), Listener {
+    override fun onEnable() {
+        logger.info("it's alive")
+        server.pluginManager.registerEvents(this, this)
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        listOf("hi", "how", "are", "you").forEach {
+            event.player.sendMessage(it)
+        }
+    }
+}
