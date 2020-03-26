a1cba4fcad79e23976fba0b1acc267535165ac49
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-03-27 01:29:48 +0200

    Automated commit

--- src/main/kotlin/org/openredstone/redstonetools/RStack.kt
+++ src/main/kotlin/org/openredstone/redstonetools/RStack.kt
@@ -2,19 +2,23 @@ package org.openredstone.redstonetools.org.openredstone.redstonetools
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Optional
 import com.sk89q.worldedit.bukkit.WorldEditPlugin
 import org.bukkit.entity.Player
 
 @CommandAlias("/rstack|/rs")
 class RStack(private val worldEdit: WorldEditPlugin) : BaseCommand() {
     @Default
+    @CommandCompletion("[-e]")
     fun rstack(
             player: Player,
+            @Optional expand: String?,
             @Default("1") times: Int,
-            @Default("false") expand: Boolean,
             @Default("2") spacing: Int
-    ): Boolean {
-        return false
+    ) {
+        val ex = expand ?: "no expand"
+        player.sendMessage("hello $ex, $times many times with spacing $spacing")
     }
 }
