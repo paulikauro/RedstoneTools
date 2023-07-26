d5245c2cbd2bf18e58d06a3c21aa89234e08ec24
Eithan Zur <eithanz713@gmail.com>
2023-07-26 23:57:36 +0300

    Changed QuickTP to not teleport player into an obstructed location, and will attempt to teleport to the closest non-obstructed location inline with the teleportation

--- src/main/kotlin/QuickTP.kt
+++ src/main/kotlin/QuickTP.kt
@@ -5,8 +5,11 @@ import co.aikar.commands.BukkitCommandCompletionContext
 import co.aikar.commands.CommandCompletions
 import co.aikar.commands.InvalidCommandArgument
 import co.aikar.commands.annotation.*
+import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.block.Block
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
 
 @CommandAlias("qtp|quicktp")
 @Description("Quick Teleport Command")
@@ -21,6 +24,14 @@ class QuickTP : BaseCommand() {
         }
         val direction = player.location.direction
         val newLocation = player.location.add(direction.multiply(distance))
+        while(!isSafeLocation(newLocation)) {
+            newLocation.subtract(direction)
+        }
         player.teleport(newLocation)
+    }
+
+    private fun isSafeLocation(location: Location): Boolean {
+        val feet = location.block
+        return !(!feet.type.isAir || !feet.location.add(0.0, 1.0, 0.0).block.type.isAir)
     }
 }
